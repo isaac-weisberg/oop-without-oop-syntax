710b71e4174fd1f5c48b9f34110245a52bdcc198
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-09-05 21:41:47 +0700

    Initial commit with a cpp class

---
+++ Car.cpp
@@ -0,0 +1,14 @@
+class Car {
+    float size;
+    void drive();
+
+    Car(float size);
+};
+
+Car::Car(float size) {
+    this->size = size;
+}
+
+void Car::drive() {
+    // Car starts going somewhere
+}
---
+++ Makefile
@@ -0,0 +1,10 @@
+
+
+default:
+	echo "Plz provide target"
+
+cpp:
+	clang -c Car.cpp
+
+c:
+	clang -c Car.c
