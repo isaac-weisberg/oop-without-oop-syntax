182d84b96bfe9493ee949fc4757ec3f779e7683e
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-09-18 15:50:36 +0700

    Add Abstraction constructors implementation to the Car.c

--- 2. Abstraction/Car.c
+++ 2. Abstraction/Car.c
@@ -1,13 +1,41 @@
+#include <stdlib.h>
+
 typedef struct {
     float bhp;
     int cylinderCount;
 } Engine;
 
+Engine* Engine_new(float bhp, int cylinderCount) {
+    Engine* this = malloc(sizeof(Engine));
+    this->bhp = bhp;
+    this->cylinderCount = cylinderCount;
+    return this;
+}
+
 typedef struct {
     int gearCount;
 } Transmission;
+
+Transmission* Transmission_new(int gearCount) {
+    Transmission* this = malloc(sizeof(Transmission));
+    this->gearCount = gearCount;
+    return this;
+}
 
 typedef struct {
     Engine* engine;
     Transmission* transmission;
 } Car;
+
+Car* Car_new(Engine* engine, Transmission* transmission) {
+    Car* this = malloc(sizeof(Car));
+    this->engine = engine;
+    this->transmission = transmission;
+    return this;
+}
+
+void usage() {
+    Engine* engine = Engine_new(150, 4);
+    Transmission* transmission = Transmission_new(5);
+    Car* car = Car_new(engine, transmission);
+}
